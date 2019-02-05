49dfb78fe42334b4af4f705b1e06cd95a514893d
Daniel <dantay0803@hotmail.co.uk>
2019-02-05 23:24:52 +0000

    Giphy clone 05/02/2019
    Add gradient animation to search button.
    
    Styles header bar.
    
    Added changing placeholder text for search bar.

--- GIPHYClone/index.js
+++ GIPHYClone/index.js
@@ -1,8 +1,11 @@
 const API_KEY = "RWL04zIh7G9Nt13nujy6YLWKLw1nZ2Mt";
 
-function getTrendingGifs() {
-  let trendingCarousel = document.querySelector("#trendingCarousel");
+let trendingCarousel;
+let homeSearchInput;
 
+let selectedPlaceholderText = 0;
+
+function getTrendingGifs() {
   fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(result => {
@@ -15,4 +18,26 @@ function getTrendingGifs() {
     .catch(error => console.log(error));
 }
 
-window.onload = getTrendingGifs();
+function placeHolderTextUpdate() {
+  const placeHolderText = [
+    "Search all the GIFs",
+    "Place GIF name here",
+    "What are you searching for"
+  ];
+
+  selectedPlaceholderText++;
+
+  if (selectedPlaceholderText >= placeHolderText.length) {
+    selectedPlaceholderText = 0;
+  }
+
+  homeSearchInput.placeholder = placeHolderText[selectedPlaceholderText];
+}
+
+window.onload = () => {
+  homeSearchInput = document.querySelector("#homeSearchInput");
+  trendingCarousel = document.querySelector("#trendingCarousel");
+
+  setInterval(placeHolderTextUpdate, 5000);
+  getTrendingGifs();
+};
